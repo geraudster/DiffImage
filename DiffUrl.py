48b75c10a020537bd582b592abc773d2136a5f22
Geraud <geraudster@gmail.com>
2012-11-27 17:31:59 +0100

    ajout du css

--- DiffUrl.py
+++ DiffUrl.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
         line = f.readline()
         
         with codecs.open('result.html', mode='w', encoding='utf-8') as result:
+            result.write('<html><head><link rel=stylesheet type=text/css href="results.css"/></head><body>')
             while line:
                 url = line.split(';')[0].strip('"')
                 name = line.split(';')[1].strip('\n').strip('"')
@@ -33,12 +34,16 @@ if __name__ == '__main__':
                 print 'Diff between %s... and %s... : %d' % (url1[0:60],  url2[0:60], rms)
                 result.write('<h1>%s</h1>' % name)
                 result.write('<span id="rms">RMS: %d</span>' %rms)
+                result.write('<div class="cartes">')
                 result.write('<div class="carte1">')
                 result.write('<a href="%s"><img src="%s"/></a>' % (url1, filename1))
                 result.write('</div>')
                 result.write('<div class="carte2">')
                 result.write('<a href="%s"><img src="%s"/></a>' % (url2, filename2))
                 result.write('</div>')
+                result.write('</div>')
                 result.write('<br/>')
                 line = f.readline()
+
+            result.write('</body></html>')
             
